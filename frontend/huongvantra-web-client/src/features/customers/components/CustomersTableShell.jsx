a2acd82244f2345115c62b1930d38ab2e420184f
Trieu Hoang Hieu <hieuwewe123@gmail.com>
2026-06-13 13:27:37 +0700

    implement seed data

--- frontend/huongvantra-web-client/src/features/customers/components/CustomersTableShell.jsx
+++ frontend/huongvantra-web-client/src/features/customers/components/CustomersTableShell.jsx
@@ -1,9 +1,7 @@
 function CustomersTableShell({ children, minWidthClass = 'min-w-[720px] xl:min-w-[840px]' }) {
   return (
-    <div className="relative hidden lg:block">
-      <div className="custom-scrollbar max-h-[min(58vh,640px)] overflow-auto overscroll-contain xl:max-h-[min(68vh,760px)]">
-        <div className={minWidthClass}>{children}</div>
-      </div>
+    <div className={`relative hidden lg:block ${minWidthClass}`}>
+      {children}
     </div>
   )
 }
